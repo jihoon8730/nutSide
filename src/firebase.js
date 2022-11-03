fc50052c6f540aaf5f262dd5cdeee49e7ab48125
신지훈 <rec8730@gmail.com>
2022-11-03 15:44:02 +0900

    Firestore 추가

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -14,4 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const authService = getAuth();
+export const db = getFirestore();
 export default app;
