e2eadb2262309fc216bbf6912e336d5f1381926a
신지훈 <rec8730@gmail.com>
2022-11-03 15:44:40 +0900

    ./firebase 경로 삭제

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
-import firebase from "./firebase";
-console.log(firebase);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
