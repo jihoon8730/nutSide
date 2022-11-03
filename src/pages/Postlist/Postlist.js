0c01f7f485cf2b2669bebc4770f4ba990db42280
신지훈 <rec8730@gmail.com>
2022-11-03 15:49:20 +0900

    Postlist(스타일목록 전체) 기능 구현

--- src/pages/Postlist/Postlist.js
+++ src/pages/Postlist/Postlist.js
@@ -1,3 +1,42 @@
-export default function Postlist() {
-  return <div>Postlist</div>;
-}
+import React, { useState, useEffect } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../../firebase";
+
+const Postlist = ({ userObj }) => {
+  console.log(userObj.uid);
+  const [userStyle, setUserStyle] = useState([]);
+  console.log(userStyle);
+
+  // getStyle Read
+  const getStyle = async () => {
+    const querySnapshot = await getDocs(collection(db, `nutside`));
+    querySnapshot.forEach((doc) => {
+      const userStyleObject = {
+        ...doc.data(),
+        id: doc.id,
+      };
+      // array return
+      setUserStyle((prev) => [userStyleObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getStyle();
+  }, []);
+
+  return (
+    <div>
+      {userStyle.map((styleInfo) => {
+        return (
+          <div key={styleInfo.createId}>
+            <div>{styleInfo.comment}</div>
+            <div>{styleInfo.top}</div>
+            <div>{styleInfo.bottom}</div>
+            <div>{styleInfo.sns}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Postlist;
