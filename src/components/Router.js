7aa4011a878bfe7cc281678ce7b0b1342cfb67c7
신지훈 <rec8730@gmail.com>
2022-11-03 15:46:06 +0900

    postlist, mylist 페이지 라우터 설정

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,10 +2,12 @@ import Navbar from "./Navbar/Navbar";
 import Home from "../pages/Home/Home";
 import Post from "../pages/Post/Post";
 import Auth from "../pages/Auth/Auth";
+import Postlist from "../pages/Postlist/Postlist";
+import Mylist from "../pages/Mylist/Mylist";
 import "normalize.css";
 import { Route, Routes } from "react-router-dom";
 
-const AppRouter = ({ isLoggedIn }) => {
+const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
     <div className="wrap">
       {isLoggedIn ? <Navbar /> : null}
@@ -15,7 +17,9 @@ const AppRouter = ({ isLoggedIn }) => {
         ) : (
           <Route path="/" element={<Auth />} />
         )}
-        <Route path="/post" element={<Post />} />
+        <Route path="/post" element={<Post userObj={userObj} />} />
+        <Route path="/postlist" element={<Postlist userObj={userObj} />} />
+        <Route path="/mylist" element={<Mylist userObj={userObj} />} />
       </Routes>
     </div>
   );
