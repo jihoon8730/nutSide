626cc4586371b658f75359a9466d4fcb5a0ee713
신지훈 <rec8730@gmail.com>
2022-11-08 02:02:31 +0900

    image URL 업로드

--- src/components/StyleEdit.js
+++ src/components/StyleEdit.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { doc, deleteDoc } from "firebase/firestore";
-import { db } from "../firebase";
+import { db, storage } from "../firebase";
+import { deleteObject, ref } from "firebase/storage";
 
 const StyleEdit = ({ myStyleObj, isOwner }) => {
-  console.log(myStyleObj);
   const onDeleteClick = () => {
     const isDelete = window.confirm("스타일을 삭제 하시겠습니까?");
     const styleRef = doc(db, "nutside", `${myStyleObj.id}`);
+    const imageFileUrl = ref(storage, myStyleObj.imageUrl);
     if (isDelete) {
       deleteDoc(styleRef);
+      deleteObject(imageFileUrl);
     }
   };
   return (
@@ -20,7 +22,12 @@ const StyleEdit = ({ myStyleObj, isOwner }) => {
           <div>{myStyleObj.outer}</div>
           <div>{myStyleObj.top}</div>
           <div>{myStyleObj.bottom}</div>
-          <div>{myStyleObj.imageFileUrl}</div>
+          <img
+            src={myStyleObj.imageUrl}
+            alt="image load fail..."
+            width="200px"
+            height="300px"
+          ></img>
           <button onClick={onDeleteClick}>Delete</button>
           <button>Edit</button>
         </>
