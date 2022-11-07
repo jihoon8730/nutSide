836c30346a118b40c8e2642a47a8901cb67a813c
신지훈 <rec8730@gmail.com>
2022-11-07 12:52:47 +0900

    기능 수정

--- src/components/StyleEdit.js
+++ src/components/StyleEdit.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { doc, deleteDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
 const StyleEdit = ({ myStyleObj, isOwner }) => {
+  console.log(myStyleObj);
+  const onDeleteClick = () => {
+    const isDelete = window.confirm("스타일을 삭제 하시겠습니까?");
+    const styleRef = doc(db, "nutside", `${myStyleObj.id}`);
+    if (isDelete) {
+      deleteDoc(styleRef);
+    }
+  };
   return (
     <div>
       {isOwner === myStyleObj.createId ? (
         <>
+          <div>{myStyleObj.sns}</div>
           <div>{myStyleObj.comment}</div>
+          <div>{myStyleObj.outer}</div>
           <div>{myStyleObj.top}</div>
           <div>{myStyleObj.bottom}</div>
-          <div>{myStyleObj.sns}</div>
-          <button>Delete</button>
+          <div>{myStyleObj.imageFileUrl}</div>
+          <button onClick={onDeleteClick}>Delete</button>
           <button>Edit</button>
         </>
       ) : null}
