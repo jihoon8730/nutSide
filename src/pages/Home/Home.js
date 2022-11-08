8c4385eab1d7d5d0632d31c9140b3401375fc1bc
신지훈 <rec8730@gmail.com>
2022-11-08 19:56:44 +0900

    home 좋아요순 뷰 구현

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,56 +1,83 @@
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { db } from "../../firebase";
+
 import "./home.scss";
 
 const Home = () => {
+  const [userStyle, setUserStyle] = useState([]);
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "nutside"),
+      orderBy("like", "desc"),
+      limit(4)
+    );
+    onSnapshot(q, (snapshot) => {
+      const userStyleArr = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setUserStyle(userStyleArr);
+    });
+  }, []);
+
   return (
     <main className="home">
       <section className="main-box">
         <article className="main-left">
-          <img
-            className="main-left-image"
-            src="./assets/no1_image.jpeg"
-            alt="no1 image load 실패"
-          />
           <div className="title-box">
             <p className="numbering-title">No.1</p>
-            <p className="social-title">0112mm_min</p>
+            <p className="social-title">{userStyle[0]?.sns}</p>
           </div>
+          <img
+            className="main-left-image"
+            src={userStyle[0]?.imageUrl}
+            alt="no1 image load 실패"
+          />
         </article>
         <article className="main-right">
           <div className="images-top">
             <div className="image-top-box-left">
-              <img
-                className="image-top-left"
-                src="./assets/no2_image.jpg"
-                alt="no2 image load 실패"
-              />
               <div className="title-box">
                 <p className="top-left-numbering-title">No.2</p>
-                <p className="top-left-social-title">jihoon308</p>
+                <p className="top-left-social-title">{userStyle[1]?.sns}</p>
               </div>
+              <img
+                className="image-top-left"
+                src={userStyle[1]?.imageUrl}
+                alt="no2 image load 실패"
+              />
             </div>
             <div className="image-top-box-right">
-              <img
-                className="image-top-right"
-                src="./assets/no3_image.jpg"
-                alt="no3 image load 실패"
-              />
               <div className="title-box">
                 <p className="top-right-numbering-title">No.3</p>
-                <p className="top-right-social-title">8730_king</p>
+                <p className="top-right-social-title">{userStyle[2]?.sns}</p>
               </div>
+              <img
+                className="image-top-right"
+                src={userStyle[2]?.imageUrl}
+                alt="no3 image load 실패"
+              />
             </div>
           </div>
           <div className="images-bottom">
             <div className="image-bottom-box">
-              <img
-                className="image-bottom-left"
-                src="./assets/no4_image.jpg"
-                alt="no4 image load 실패"
-              />
               <div className="title-box">
                 <p className="bottom-numbering-title">No.4</p>
-                <p className="bottom-social-title">rec8730</p>
+                <p className="bottom-social-title">{userStyle[3]?.sns}</p>
               </div>
+              <img
+                className="image-bottom-left"
+                src={userStyle[3]?.imageUrl}
+                alt="no4 image load 실패"
+              />
             </div>
             <div className="title-bottom-right">
               <p>너의</p>
