7a52341806be17619caa51fc7b89754f91cdd6f3
신지훈 <rec8730@gmail.com>
2022-11-08 19:57:38 +0900

    navbar 링크 구현

--- src/components/Navbar/Users.js
+++ src/components/Navbar/Users.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import "./users.scss";
 
 const Users = () => {
   const users = useNavigate();
   const goToUsers = () => {
     users("/postlist");
   };
-  return <div onClick={goToUsers}>Users</div>;
+  return (
+    <div className="Users" onClick={goToUsers}>
+      Users
+    </div>
+  );
 };
 
 export default Users;
