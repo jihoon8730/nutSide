cfd32887c1d0bb55b3afbe0f6361f10174dc6967
신지훈 <rec8730@gmail.com>
2022-11-14 18:53:01 +0900

    리스트에서 좋아요 갯수 출력 기능 추가

--- src/pages/Postlist/Postlist.js
+++ src/pages/Postlist/Postlist.js
@@ -2,13 +2,25 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { db } from "../../firebase";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./postlist.scss";
 
 const Postlist = ({ userStyle, setUserStyle }) => {
   const goToDetail = useNavigate();
-
   const onLikeTopSort = async () => {
     const q = query(collection(db, "nutside"), orderBy("like", "desc"));
+    onSnapshot(q, (snapshot) => {
+      const userStyleArr = snapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      setUserStyle(userStyleArr);
+    });
+  };
+
+  const onCreateDateSort = async () => {
+    const q = query(collection(db, "nutside"), orderBy("createAt", "desc"));
     onSnapshot(q, (snapshot) => {
       const userStyleArr = snapshot.docs.map((doc) => ({
         id: doc.id,
@@ -22,7 +34,10 @@ const Postlist = ({ userStyle, setUserStyle }) => {
     <div className="Postlist">
       <div className="like-rank">
         <button className="like-rank-btn" onClick={onLikeTopSort}>
-          좋아요순
+          인기순
+        </button>
+        <button className="create-rank-btn" onClick={onCreateDateSort}>
+          등록순
         </button>
       </div>
       <div className="postlist-box">
@@ -35,7 +50,14 @@ const Postlist = ({ userStyle, setUserStyle }) => {
                 goToDetail(`/detail/${styleInfo.id}`);
               }}
             >
-              <div className="postlist-title">{styleInfo.sns}</div>
+              <div className="postlist-title">
+                {styleInfo.sns}
+                <div className="title-like">
+                  <FontAwesomeIcon icon={faHeart} />
+                  {styleInfo.like}
+                </div>
+              </div>
+
               <img
                 className="postlist-image"
                 src={styleInfo.imageUrl}
