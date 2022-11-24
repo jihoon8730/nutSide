f9c0aaa90263e15792420e390d03b38c8bbe2430
신지훈 <rec8730@gmail.com>
2022-11-24 17:30:12 +0900

    Detail 페이지 삭제기능 추가

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -8,17 +8,21 @@ import {
   query,
   collection,
   orderBy,
+  deleteDoc,
 } from "firebase/firestore";
 import { useParams } from "react-router-dom";
-import { db } from "../../firebase";
+import { db, storage } from "../../firebase";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faHeart as farHeart } from "@fortawesome/free-regular-svg-icons";
-import "./detail.scss";
 import CommentInput from "./CommentInput";
+import { deleteObject, ref } from "firebase/storage";
+import { useNavigate } from "react-router-dom";
+import "./detail.scss";
 
 const Detail = ({ userObj, userStyle }) => {
   const [userComments, setUserComments] = useState();
+  const goToList = useNavigate();
 
   let { id } = useParams();
   let userId = userStyle.find((style) => {
@@ -39,7 +43,6 @@ const Detail = ({ userObj, userStyle }) => {
       setUserComments(userCommentArr);
     });
   }, [userStyle]);
-  console.log(userStyleDoc);
 
   const isLike = userId?.likelist.includes(userObj.uid);
   const onLikeCount = async () => {
@@ -56,6 +59,16 @@ const Detail = ({ userObj, userStyle }) => {
     }
   };
 
+  const onDeleteStyle = () => {
+    const isDeleteConfirm = window.confirm("정말로 스타일을 삭제하시겠습니까?");
+    const styleDelete = doc(db, "nutside", `${userId?.id}`);
+    const imageDelete = ref(storage, userStyle.imageUrl);
+    if (isDeleteConfirm) {
+      goToList("/postlist");
+      deleteDoc(styleDelete);
+      deleteObject(imageDelete);
+    }
+  };
   return (
     <div className="Detail">
       <a
@@ -73,6 +86,14 @@ const Detail = ({ userObj, userStyle }) => {
           alt="image URL load fail..."
           width="200px"
         />
+        <button
+          className="detail-delete"
+          onClick={() => {
+            onDeleteStyle();
+          }}
+        >
+          삭제
+        </button>
         <div className="detail-contents">
           <div>TOP : {userId?.top}</div>
           <div>Bottom : {userId?.bottom}</div>
